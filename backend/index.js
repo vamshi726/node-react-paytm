31a26c09e3b92fbd5603fcac1715c54611c56dd5
Vamshi Muluguri <vamshimuluguri@gmail.com>
2024-07-16 18:17:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ const cors = require("cors");
 const rootRouter = require("./routes/index");
 
 const app = express();
-
+const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
-app.listen(3000);
+
+app.listen(()=>{
+    console.log("Server listening on port : "+ port);
+}) 
